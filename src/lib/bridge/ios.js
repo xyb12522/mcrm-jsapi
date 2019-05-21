5f619800a72838ac829e63f84d96508953a6ab15
xyb12522 <845531877@qq.com>
2019-05-21 16:26:59 +0800

    update iosbridge

--- src/lib/bridge/ios.js
+++ src/lib/bridge/ios.js
@@ -46,10 +46,6 @@ var h5IosBridge = function(namespace, callObj) {
             var bridgeFunc = (bridge) => {
                 bridge.callHandler(namespace, Object.assign({}, obj), function(data) {
                     data = data || {};
-                    var text =document.createElement("TEXT");
-                    var t = document.createTextNode(JSON.stringify(data));
-                    text.appendChild(t);
-                    document.querySelector('#message').appendChild(text);
                     if (data.code == "0") {
                         if (typeof obj.onSuccess === "function") {
                             obj.onSuccess.call(null, data.msg);
